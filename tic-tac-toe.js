c25722a3328786d1d8f38e02513acbff8ea2ba6c
David Wise <davidwise964@gmail.com>
2023-10-31 21:47:06 +0600

    Add getInitialBoard function
    Summary:
    * I want to start writing a tic-tac-toe game in Node.js, and this commit starts the game by writing a function to print out a board.
    * The getInitialBoard function has modifiable constants H and W for the height and width of the board, and prints out a text tic-tac-toe board based on those (see code comments for an example).
    
    Test plan:
    * Ran `node tic-tac-toe.js` and the board was as expected.

---
+++ tic-tac-toe.js
@@ -0,0 +1,54 @@
+
+/* Produces a diagram that looks like this:
+   1   2   3
+A    |   |
+  -----------
+B    |   |
+  -----------
+C    |   |   
+*/
+function getInitialBoard() {
+  // If height is larger than 9, we need to adjust the label widths
+  // to account for two-digit labels:
+  const H = 3;
+  // If the width is larger than 26, we need to adjust the column labels
+  // to account for labels after Z:
+  const W = 3;
+
+  let boardString = '';
+
+  // Column labels:
+  for (let j = 0; j < W; j += 1) {
+    boardString += '   ' + String.fromCharCode('A'.charCodeAt(0) + j);
+  }
+  boardString += '\n';
+
+  // Rest of the board:
+  for (let i = 0; i < H; i += 1) {
+    if (i > 0) {
+      // Add a divider line:
+      for (let j = 0; j < W; j += 1) {
+        if (j === 0) {
+          boardString += '  ---';
+        } else {
+          boardString += '----';
+        }
+      }
+      boardString += '\n';
+    }
+
+    // Add a line with a row label and column dividers:
+    for (let j = 0; j < W; j += 1) {
+      if (j === 0) {
+        boardString += (i + 1).toString() + '    ';
+      } else {
+        boardString += '|   ';
+      }
+    }
+    boardString += '\n';
+  }
+
+  return boardString;
+}
+
+console.log(getInitialBoard());
